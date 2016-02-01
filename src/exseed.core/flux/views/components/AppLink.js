7039c9f8b92b035f045c5ee7b62e5264e52780fe
gocreating <gocreating@gmail.com>
2016-02-01 14:41:10 +0800

    Fix bug

--- src/exseed.core/flux/views/components/AppLink.js
+++ src/exseed.core/flux/views/components/AppLink.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export function createLinkOf(appName) {
-  return class ExseedLink extends React.Component {
+  return class AppLink extends React.Component {
     render() {
       if (this.props.app === appName) {
-        return <Link to={this.props.to}>{this.props.children}</Link>;
-      } else {
-        return <a href={this.props.to}>{this.props.children}</a>;
+        return (
+          <Link
+            {...this.props}
+            to={this.props.to}>
+            {this.props.children}
+          </Link>
+        );
       }
+      return (
+        <a {...this.props} href={this.props.to}>
+          {this.props.children}
+        </a>
+      );
     }
   };
 };
